9eae9255a570a526fa6776eb3155fd6a6d929952
gowtham-178 <gowthamdakinedi@gmail.com>
2026-03-07 14:57:29 +0530

    new file:   client/src/config.js

---
+++ client/src/config.js
@@ -0,0 +1,2 @@
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ? `${process.env.REACT_APP_API_BASE_URL}/api` : undefined;
+export const API_SERVER_URL = process.env.REACT_APP_API_BASE_URL;
